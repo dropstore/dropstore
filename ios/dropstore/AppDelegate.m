8e6d8f001abbe08e71fb8bf35492bc7d4bcb9d0c
zhangweiwang <fighting_android@sina.com>
2019-08-20 17:47:11 +0800

    iOS集成支付宝和微信支付

--- ios/dropstore/AppDelegate.m
+++ ios/dropstore/AppDelegate.m
@@ -10,7 +10,9 @@
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
-
+#import <AlipaySDK/AlipaySDK.h>
+#import "RNSplashScreen.h"
+#import <React/RCTLinkingManager.h>
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
@@ -27,6 +29,7 @@
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
+  [RNSplashScreen show];
   return YES;
 }
 
@@ -37,6 +40,65 @@
 #else
   return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
 #endif
+}
+
+
+//支付回调9以后
+- (BOOL)application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary*)options {
+  
+  if ([url.host isEqualToString:@"safepay"]) {
+    //跳转支付宝钱包进行支付，处理支付结果
+    [[AlipaySDK defaultService] processOrderWithPaymentResult:url standbyCallback:^(NSDictionary *resultDic) {
+      NSNotification * notification = [NSNotification notificationWithName:@"Alipay" object:nil userInfo:resultDic];
+      [[NSNotificationCenter defaultCenter] postNotification:notification];
+    }];
+  }
+  return  [WXApi handleOpenURL:url delegate:self];
+}
+
+//支付回调9以前
+- (BOOL)application:(UIApplication *)application handleOpenURL:(NSURL *)url {
+  return  [WXApi handleOpenURL:url delegate:self];
+}
+
+
+
+//ios9以后的方法
+- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation {
+  
+  if ([url.host isEqualToString:@"safepay"]) {
+    //跳转支付宝钱包进行支付，处理支付结果
+    [[AlipaySDK defaultService] processOrderWithPaymentResult:url standbyCallback:^(NSDictionary *resultDic) {
+      NSNotification * notification = [NSNotification notificationWithName:@"Alipay" object:nil userInfo:resultDic];
+      [[NSNotificationCenter defaultCenter] postNotification:notification];
+    }];
+    return YES;
+  }
+  return [WXApi handleOpenURL:url delegate:self];
+}
+
+- (BOOL)application:(UIApplication *)application continueUserActivity:(nonnull NSUserActivity *)userActivity restorationHandler:(nonnull void (^)(NSArray<id<UIUserActivityRestoring>> * _Nullable))restorationHandler{
+  return [RCTLinkingManager application:application
+                   continueUserActivity:userActivity
+                     restorationHandler:restorationHandler];
+}
+
+#pragma mark - wx callback
+
+- (void) onReq:(BaseReq*)req
+{
+  // TODO Something
+}
+
+- (void)onResp:(BaseResp *)resp
+{
+  //判断是否是微信支付回调 (注意是PayResp 而不是PayReq)
+  if ([resp isKindOfClass:[PayResp class]])
+  {
+    //发出通知 从微信回调回来之后,发一个通知,让请求支付的页面接收消息,并且展示出来,或者进行一些自定义的展示或者跳转
+    NSNotification * notification = [NSNotification notificationWithName:@"WXPay" object:nil userInfo:@{@"errCode":@(resp.errCode)}];
+    [[NSNotificationCenter defaultCenter] postNotification:notification];
+  }
 }
 
 @end
