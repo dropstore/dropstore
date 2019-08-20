fd07b10fafe5b54730479bcce2c85eb750936325
zhangchao <zhangchao@dengdengfu.com>
2019-08-20 17:30:19 +0800

    整理字体

--- app/res/FontFamily.js
+++ app/res/FontFamily.js
@@ -1,6 +1,5 @@
-const FONT = {
-  Normal: 'PingFangSC-Regular', // 普通字体
-  YaHei: 'PingFangSC-Semibold', // 微软雅黑替代字体
-  Mario: 'Super Mario World Text Box', // 马里奥(像素)字体
-};
-export default FONT;
+const Normal = 'PingFangSC-Regular';// 普通字体
+const YaHei = 'PingFangSC-Semibold';// 微软雅黑替代字体
+const Mario = 'Super Mario World Text Box';// 马里奥(像素)字体
+
+export { Normal, YaHei, Mario };
