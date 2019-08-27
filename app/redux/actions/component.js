407ff9f8621e38e2559d3b6ee1dabdc78ba8d17f
zhangchao <zhangchao@dengdengfu.com>
2019-08-27 15:44:52 +0800

    修复分享

--- app/redux/actions/component.js
+++ app/redux/actions/component.js
@@ -1,7 +1,16 @@
 import { createAction } from 'redux-actions';
 
 const closeShare = createAction('CLOSE_SHARE');
-const showShare = createAction('SHOW_SHARE');
+const setShowShare = createAction('SHOW_SHARE');
 const shareCallback = createAction('SHARE_CALLBACK');
 
-export { closeShare, showShare, shareCallback };
+function showShare(params) {
+  return (dispatch) => {
+    dispatch(closeShare());
+    dispatch(setShowShare(params));
+  };
+}
+
+export {
+  closeShare, showShare, shareCallback, setShowShare,
+};
