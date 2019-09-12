f91e96a972c8bfa3086e6554344796e3c196402d
zhangchao <zhangchao@dengdengfu.com>
2019-09-12 13:14:22 +0800

    s

--- app/page/commission/index.js
+++ app/page/commission/index.js
@@ -3,21 +3,22 @@
  * @date 2019/8/22 22:26
  * @author ZWW
  */
-import React, {PureComponent} from 'react';
-import {StyleSheet, Text, TextInput, View} from 'react-native';
-import {withNavigation} from 'react-navigation';
-import {connect} from "react-redux";
-import ImageBackground from '../../components/ImageBackground';
-import {SCREEN_WIDTH} from '../../common/Constant';
+import React, { PureComponent } from 'react';
+import {
+  StyleSheet, Text, TextInput, View,
+} from 'react-native';
+import { withNavigation } from 'react-navigation';
+import { connect } from 'react-redux';
+import { ImageBackground, KeyboardDismiss } from '../../components';
+import { SCREEN_WIDTH, PADDING_TAB } from '../../common/Constant';
 import Images from '../../res/Images';
 import Colors from '../../res/Colors';
-import {Normal, YaHei} from '../../res/FontFamily';
-import {debounce} from '../../utils/commonUtils';
-import {getShopDetailInfo} from "../../redux/reselect/shopDetailInfo";
-import {setCommission, getPayMes} from "../../redux/actions/shopDetailInfo";
-import {bottomStyle} from "../../res/style/BottomStyle";
-import {showToast} from "../../utils/MutualUtil";
-import ShopConstant from "../../common/ShopConstant";
+import { Normal, YaHei } from '../../res/FontFamily';
+import { debounce } from '../../utils/commonUtils';
+import { getShopDetailInfo } from '../../redux/reselect/shopDetailInfo';
+import { setCommission, getPayMes } from '../../redux/actions/shopDetailInfo';
+import { bottomStyle } from '../../res/style/BottomStyle';
+import ShopConstant from '../../common/ShopConstant';
 
 function mapStateToProps() {
   return state => ({
@@ -32,56 +33,58 @@ class Commission extends PureComponent {
       totalPrice: 0,
       number: 0,
       commission: 0,
-    }
+    };
   }
 
   componentDidMount() {
-    const {shopDetailInfo} = this.props;
+    const { shopDetailInfo } = this.props;
     const shopInfo = shopDetailInfo.data;
-    getPayMes(shopInfo.activity.id, shopInfo.user_activity.id).then(res => {
-      let data = res.data;
+    getPayMes(shopInfo.activity.id, shopInfo.user_activity.id).then((res) => {
+      const data = res.data;
       if (data) {
-        this.setState({number: data.number, commission: data.commission})
+        this.setState({ number: data.number, commission: data.commission });
       }
-    })
+    });
   }
 
   _toPay = () => {
-    const {shopDetailInfo, navigation} = this.props;
+    const { shopDetailInfo, navigation } = this.props;
     const shopInfo = shopDetailInfo.data;
     // let minPrice = shopInfo.activity.min_price;
-    let commission = this.state.commission;
+    const commission = this.state.commission;
     if (commission < 1) {
       // return showToast(`单人佣金不能低于1元`);
     }
-    let acId = shopInfo.user_activity.id;
-    setCommission(shopInfo.activity.id, acId, commission).then(res => {
+    const acId = shopInfo.user_activity.id;
+    setCommission(shopInfo.activity.id, acId, commission).then((res) => {
       if (res) {
-        let payData = {
-          'order_id': acId,
-          'price': res.data / 100
+        const payData = {
+          order_id: acId,
+          price: res.data / 100,
         };
         navigation.navigate('pay', {
           title: '选择支付账户',
           type: ShopConstant.PAY_COMMISSION,
-          payData: payData,
-          shopInfo: shopInfo
+          payData,
+          shopInfo,
         });
       }
-    })
+    });
   };
 
   onChange = (event) => {
     const singlePrice = event.nativeEvent.text;
-    this.setState({commission: singlePrice, totalPrice: singlePrice * this.state.number})
+    this.setState({ commission: singlePrice, totalPrice: singlePrice * this.state.number });
   };
 
   render() {
     return (
-      <View style={_styles.container}>
+      <KeyboardDismiss style={_styles.container}>
         <View style={_styles.mainView}>
-          <Text style={_styles.countTitle}>合计数量
-            <Text style={_styles.count}> {this.state.number}</Text> 双
+          <Text style={_styles.countTitle}>
+            {'合计数量 '}
+            <Text style={_styles.count}>{this.state.number}</Text>
+            {' 双'}
           </Text>
           <ImageBackground source={Images.framePhoneInput} style={_styles.inputBg}>
             <TextInput
@@ -104,17 +107,17 @@ class Commission extends PureComponent {
         <View style={_styles.bottomView}>
           <View style={_styles.bottomLeftView}>
             <Text style={_styles.payTitle}>合计金额:</Text>
-            <Text style={_styles.price}>{this.state.totalPrice}￥</Text>
+            <Text style={_styles.price}>{`${this.state.totalPrice}￥`}</Text>
           </View>
           <ImageBackground
-            style={bottomStyle.buttonOnlyOneChildView}
+            style={_styles.buttonOnlyOneChildView}
             source={Images.bg_right}
             onPress={debounce(this._toPay)}
           >
             <Text style={bottomStyle.buttonText}>确认</Text>
           </ImageBackground>
         </View>
-      </View>
+      </KeyboardDismiss>
     );
   }
 }
@@ -122,12 +125,12 @@ class Commission extends PureComponent {
 const _styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: Colors.WHITE_COLOR
+    backgroundColor: Colors.WHITE_COLOR,
   },
   mainView: {
     flex: 1,
     marginTop: 177,
-    marginLeft: 74
+    marginLeft: 74,
   },
   countTitle: {
     fontSize: 13,
@@ -148,7 +151,7 @@ const _styles = StyleSheet.create({
   },
   inputBg: {
     width: 259,
-    height: 60
+    height: 60,
   },
   pricePh: {
     flex: 1,
@@ -164,23 +167,31 @@ const _styles = StyleSheet.create({
   },
   bottomView: {
     width: SCREEN_WIDTH,
-    height: 61,
+    height: 61 + PADDING_TAB,
+    paddingBottom: PADDING_TAB,
     flexDirection: 'row',
     alignItems: 'center',
     borderTopWidth: 1,
+    paddingHorizontal: 10,
     borderTopColor: 'rgba(215, 215, 215, 1)',
   },
   bottomLeftView: {
     flex: 1,
     flexDirection: 'row',
-    alignItems: 'center'
+    alignItems: 'center',
   },
   price: {
     fontSize: 29,
     fontFamily: YaHei,
     fontWeight: '400',
     color: 'rgba(0,0,0,1)',
-    marginLeft: 10
+    marginLeft: 10,
+  },
+  buttonOnlyOneChildView: {
+    width: 178,
+    height: 48,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   bottomRightView: {
     width: 178,
@@ -189,13 +200,13 @@ const _styles = StyleSheet.create({
     alignItems: 'center',
     marginVertical: 6,
     marginRight: 7,
-    marginLeft: 23
+    marginLeft: 23,
   },
   buttonText: {
     fontSize: 16,
     fontFamily: Normal,
-    color: 'rgba(255,255,255,1)'
-  }
+    color: 'rgba(255,255,255,1)',
+  },
 });
 
 export default connect(mapStateToProps)(withNavigation(Commission));
