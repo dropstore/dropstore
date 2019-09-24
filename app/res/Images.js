6b78355590e17a229e63fb82542d27309ec7c000
zhangchao <zhangchao@dengdengfu.com>
2019-09-24 19:19:48 +0800

    s

--- app/res/Images.js
+++ app/res/Images.js
@@ -18,17 +18,11 @@ const IMAGES = {
   back: require('./image/back.png'),
   ic_back_gray: require('./image/ic-back-gray.png'),
   loading: require('./image/loading.gif'),
-  xh: require('./image/xh.png'),
-  xm: require('./image/xm.png'),
-  shape: require('./image/Shape.png'),
   lot_win_rate: require('./image/lot_win_rate.png'),
 
-  // 底部button
-  tzw: require('./image/tzw.png'),
   // 分享相关
   wx: require('./image/wx.png'),
   pyq: require('./image/pyq.png'),
-  wb: require('./image/wb.png'),
   // 登陆相关
   nameAge: require('./image/name-age.png'),
   sizeGender: require('./image/size-gender.png'),
@@ -36,20 +30,8 @@ const IMAGES = {
   girl: require('./image/girl.png'),
   iconBoy: require('./image/icon-boy.png'),
   iconGirl: require('./image/icon-girl.png'),
-  framePhoneInput: require('./image/frame-phone-input.png'),
-  verifiCode: require('./image/verifi-code.png'),
-  frameNickname: require('./image/frame-nickname.png'),
-  frameRed: require('./image/frame-red.png'),
-  frameBlack: require('./image/frame-black.png'),
-  frameSize: require('./image/frame-size.png'),
   sexText: require('./image/sex-text.png'),
   phoneNum: require('./image/phoneNum.png'),
-
-  // 选择鞋码
-  shoe_zjt: require('./image/shoe_zjt.png'),
-  shoe_zjr: require('./image/shoe_zjr.png'),
-  shoe_hth: require('./image/shoe_hth.png'),
-  close_shoe: require('./image/chazi.png'),
 
   // 支付界面
   pay_zfb: require('./image/pay_zfb.png'),
@@ -64,14 +46,9 @@ const IMAGES = {
   zf_sb: require('./image/zfsb.png'),
   got_em: require('./image/got_em.png'),
   // 活动成员列表
-  xt_xn: require('./image/xtxn.png'),
   shape_1_ji3: require('./image/shape_1_ji3.png'),
-  tx: require('./image/tx.png'),
   // 个人中心
-  frameHead: require('./image/frame-head.png'),
-  frameWallet: require('./image/frame-wallet.png'),
   wallet: require('./image/wallet.png'),
-  uncomplete: require('./image/uncomplete.png'),
   myActivity: require('./image/myActivity.png'),
   myWarehouse: require('./image/myWarehouse.png'),
   extract: require('./image/extract.png'),
@@ -79,21 +56,13 @@ const IMAGES = {
   helper: require('./image/helper.png'),
   fashounotice: require('./image/fashounotice.png'),
   systemnotice: require('./image/systemnotice.png'),
-  setting: require('./image/setting.png'),
   safesetting: require('./image/safesetting.png'),
   littleBoy: require('./image/little-boy.png'),
   littleGirl: require('./image/little-girl.png'),
   // 个人设置
-  frameAvatar: require('./image/frameAvatar.png'),
   iconRight: require('./image/icon-arrow-right.png'),
   chooseBoy: require('./image/choose-boy.png'),
   chooseGirl: require('./image/choose-girl.png'),
-  // 地址
-  xiaoJiaHao: require('./image/xiaojiah.png'),
-  frameAddressEdit: require('./image/frame-address-edit.png'),
-  shanchu: require('./image/shanchu.png'),
-  bianji: require('./image/bianji.png'),
-  frameAddres: require('./image/frameAddres.png'),
 
   // 自由贸易
   freeTradePlaceholder: require('./image/freeTrade-placeholder.png'),
