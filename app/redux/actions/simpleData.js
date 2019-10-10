ffbc6f345384907b84cd7dafb25aa335ba9098ff
zhangchao <zhangchao@dengdengfu.com>
2019-10-10 17:12:36 +0800

    s

--- app/redux/actions/simpleData.js
+++ app/redux/actions/simpleData.js
@@ -7,8 +7,8 @@ const requestSimpleData = createAction('REQUEST_SIMPLE_DATA', a => a, (a, needCl
 const resetAllSimpleData = createAction('RESET_ALL_SIMPLE_DATA');
 
 function fetchSimpleData(type = '', query = {}, fetchType: 'reload' | 'refresh' = null) {
-  return (dispatch, getState) => new Promise((resolve) => {
-    if (!fetchType && JSON.stringify(query) === JSON.stringify((getState().simpleData[type] || {}).fetchedParams)) { return; }
+  return dispatch => new Promise((resolve) => {
+    // if (!fetchType && JSON.stringify(query) === JSON.stringify((getState().simpleData[type] || {}).fetchedParams)) { return; }
     dispatch(requestSimpleData(type, fetchType === 'reload'));
     const params = {
       ...api[type].initParams,
