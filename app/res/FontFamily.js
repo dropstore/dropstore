36cab6662e3e1d517d6a5d12b21fe6a70ac214a4
zhangchao <zhangchao@dengdengfu.com>
2019-10-12 18:29:32 +0800

    s

--- app/res/FontFamily.js
+++ app/res/FontFamily.js
@@ -4,7 +4,7 @@ const Mario = 'Aldrich-Regular';// 马里奥(像素)字体
 const Aldrich = 'Aldrich-Regular';// 倒计时字体
 const BlackItalic = 'Roboto-BlackItalic';// 金额字体
 const RuiXian = 'ruixian';// 金额字体
-const BrowalliaNew = 'ruixian';// 金额字体
+const BrowalliaNew = 'PingFangSC-Semibold';// 金额字体
 
 export {
   Normal, YaHei, Mario, Aldrich, BlackItalic, RuiXian, BrowalliaNew,
