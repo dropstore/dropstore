a366f0015bc9d21a2350c12c92bc2c6abf686f22
zhangchao <zhangchao@dengdengfu.com>
2019-10-16 11:56:05 +0800

    s

--- app/page/identify/index.js
+++ app/page/identify/index.js
@@ -1,16 +1,16 @@
 import React, { PureComponent } from 'react';
 import { View, StyleSheet } from 'react-native';
 import NavigationBarCom from '../../components/NavigationBarCom';
-import Images from '../../res/Images';
 import Image from '../../components/Image';
-import { wPx2P } from '../../utils/ScreenUtil';
+import { wPx2P, hPx2P } from '../../utils/ScreenUtil';
 
 export default class Identify extends PureComponent {
   render() {
     return (
       <View style={styles.container}>
         <NavigationBarCom title="鉴定" />
-        <Image style={{ width: wPx2P(480 / 2), height: wPx2P(190 / 2) }} source={Images.identifyPlaceholder} />
+        <Image style={styles.chaofan_hui} source={require('../../res/image/chaofan_hui.png')} />
+        <Image style={styles.jianding_hui} source={require('../../res/image/jianding_hui.png')} />
       </View>
     );
   }
@@ -21,5 +21,11 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     flex: 1,
+  },
+  jianding_hui: {
+    width: wPx2P(238), height: wPx2P(52), marginTop: hPx2P(20), marginBottom: hPx2P(50),
+  },
+  chaofan_hui: {
+    width: wPx2P(59), height: wPx2P(68),
   },
 });
