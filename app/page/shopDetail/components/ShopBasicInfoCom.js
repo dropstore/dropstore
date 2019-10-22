569585ccb68acf5e94ed535cba77e27f040e69bc
zhangchao <zhangchao@dengdengfu.com>
2019-10-22 13:36:15 +0800

    s

--- app/page/shopDetail/components/ShopBasicInfoCom.js
+++ app/page/shopDetail/components/ShopBasicInfoCom.js
@@ -93,7 +93,7 @@ const styles = StyleSheet.create({
     marginBottom: 10,
   },
   shopTitle: {
-    fontSize: 13,
+    fontSize: 14,
     color: 'rgba(0,0,0,1)',
     fontFamily: YaHei,
     marginTop: 9,
