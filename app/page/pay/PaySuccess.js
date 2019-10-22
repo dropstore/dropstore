4a6af8597c7835fd1adad161a3bff57f2911b4b8
zhangchao <zhangchao@dengdengfu.com>
2019-10-22 10:09:27 +0800

    s

--- app/page/pay/PaySuccess.js
+++ app/page/pay/PaySuccess.js
@@ -49,7 +49,7 @@ export default class PaySuccess extends PureComponent {
       buyActivityGoods: `${baseUrl}?id=${shopInfo.order_id}`,
       commission: `${baseUrl}?id=${aId}&u_a_id=${uAId}&activity_id=${aId}&inviter=${uId}`,
     }[payType];
-    const title = payType === 'buyActivityGoods' ? '差购买成功文案' : shopInfo.activity.b_type === '2'
+    const title = payType === 'buyActivityGoods' ? '差购买成功文案' : shopInfo.activity?.b_type === '2'
       ? `快来炒饭APP帮我助攻抢购，成功可立获${shopInfo.user_activity.commission / 100}元佣金`
       : `快来炒饭APP帮我抽一支幸运签，中签可立获${shopInfo.user_activity.commission / 100}元佣金`;
     showShare({
@@ -172,7 +172,7 @@ export default class PaySuccess extends PureComponent {
     return (
       <View style={styles.container}>
         <ScrollView contentContainerStyle={{ flex: 1, alignItems: 'center', paddingTop: 10 }} showsVerticalScrollIndicator={false}>
-          <View style={{ alignItems: 'center' }}>
+          <View style={{ alignItems: 'center', marginTop: STATUSBAR_HEIGHT + 20 }}>
             <FadeImage style={styles.goodImage} source={{ uri: shopInfo.goods.image }} />
             <Image style={styles.icon} source={require('../../res/image/chaofan_hui.png')} />
             <Text style={styles.shopName}>{shopInfo.goods.goods_name}</Text>
@@ -209,7 +209,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: Colors.WHITE_COLOR,
-    paddingTop: STATUSBAR_HEIGHT + 20,
   },
   hengxian: {
     backgroundColor: '#E2E2E2',
