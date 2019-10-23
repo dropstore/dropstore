166c7aaafd2bfb44e3e5b63fe21c498d37e64d85
zhangchao <zhangchao@dengdengfu.com>
2019-10-23 18:02:45 +0800

    s

--- app/page/shopDetail/components/SelectShoeSizeCom.js
+++ app/page/shopDetail/components/SelectShoeSizeCom.js
@@ -90,7 +90,7 @@ class SelectShoeSizeCom extends Component {
     closeBox();
     request('/activity/do_add_user_activity', { params, isShowLoading: true }).then(() => {
       fetchSimpleData('activityInfo', { id: shopId }, 'refresh').then((data) => {
-        if (data.user_activity.number > 1) {
+        if (data.user_activity.want_number > 0) {
           navigation.push('commission', { title: '助攻佣金设定' });
         }
       });
