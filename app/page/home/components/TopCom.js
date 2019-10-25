45a8f6075d5b0ca13524c44496f265c9f4f1457d
zhangchao <zhangchao@dengdengfu.com>
2019-10-25 18:10:18 +0800

    s

--- app/page/home/components/TopCom.js
+++ app/page/home/components/TopCom.js
@@ -63,7 +63,7 @@ class TopCom extends PureComponent {
 const _styles = StyleSheet.create({
   topImage: {
     width: wPx2P(375),
-    height: wPx2P(138),
+    height: wPx2P(180),
     alignSelf: 'center',
   },
 });
